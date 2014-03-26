a6ae6d6c76fd239f2f1e26ce7e966cf7f88bd012
Christoph Malek <malekchristoph@gmail.com>
2014-03-26 17:58:12 +0100

    added main_site

---
+++ web/include/twsmw/main_site.hpp
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 2014 Christoph Malek
+// See LICENSE for more information.
+//
+
+#ifndef TWSMW_MAIN_SITE_HPP
+#define TWSMW_MAIN_SITE_HPP
+
+
+namespace twsmw
+{
+	class main_site
+	{
+
+	};
+}
+
+
+#endif // TWSMW_MAIN_SITE_HPP
