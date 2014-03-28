fdaca3b737c3aa23a853640f8e7562d4b6ddbe18
Christoph Malek <malekchristoph@gmail.com>
2014-03-28 22:42:31 +0100

    main.cpp

--- web/src/twsmw/main.cpp
+++ web/src/twsmw/main.cpp
@@ -3,8 +3,10 @@
 // See LICENSE for more information.
 //
 
+#include <twsmw/main_site.hpp>
+
 
 int main(int argc, char* argv[])
 {
-
+	return Wt::WRun(argc, argv, [](const Wt::WEnvironment& e){return new twsmw::main_site{e};});
 }
